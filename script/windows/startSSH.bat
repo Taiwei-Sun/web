f6a822f441f670bdb30f1901225ddd0f81404b24
Taiwei <ghnbsun2011@gmail.com>
2018-11-12 02:23:02 +0800

    1.modify otherTag.txt  2.modify Git  3.add gnu_assembler  4.add startSSH.bat

---
+++ script/windows/startSSH.bat
@@ -0,0 +1,7 @@
+echo off
+title start vmware ssh center
+:: See the title at the top. And this comment will not appear in the command prompt.
+::echo Test file executed.
+::echo I am too lazy to write commands again and again.
+::pause
+vmrun start "C:\Users\perry\Documents\Virtual Machines\CentOS 64-bit-ssh\CentOS 64-bit-ssh.vmx"
